4099a192223c7e97b9349a0dc6cf5bd977eebaa8
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-04-12 14:09:31 +0300

    Diagrammid ja graafikud

--- Failidega_too.py
+++ Failidega_too.py
@@ -14,24 +14,38 @@ def loe_failist(fail:str)->list:
 def kirjuta_failisse(fail:str,jarjend=[]):
     """
     """
-    n=int(input("Sisesta mitu elemendi: "))
-    for i in range(n):
-        jarjend.append(input(f"{i+1}. element: "))
+    #n=int(input("Sisesta mitu elemendi: "))
+    #for i in range(n):
+    #    jarjend.append(input(f"{i+1}. element: "))
     f=open(fail,'w',encoding="utf-8")
     for el in jarjend:
         f.write(el+"\n")
     f.close()
+def kirjuta_log_pas(fail:str,jarjend1=[],jarjend2=[]):
+    """
+    """
+    f=open(fail,'w',encoding="utf-8")
+    for i in range(len(jarjend1)):
+        f.write(jarjend1[i]+":"+jarjend2[i]+"\n")
+    f.close()
+def loe_pas_ja_log(fail:str)->any:
+    """Loeb failist andmed, mis oli sisestatud formaadis "login:password" igas reas eraldi
+    """
+    fail=open(fail,"r",encoding="utf-8")
+    log=[]
+    pas=[]
+    #log_pas={}
+    for line in fail:
+        n=line.find("-")# login:password - разделитель
+        log.append(line[0:n].strip())
+        pas.append(line[n+1:len(line)].strip())    
+        #l,p=line.strip().split("-")
+        #log_pas[l]=p        
+    fail.close()
+    return log,pas #,log_pas
 
 def heli(tekst:str,keel:str):
     obj=gTTS(text=tekst,lang=keel,slow=False).save("heli.mp3")
     system("heli.mp3")
 
-tekst=input("Sisesta tekst: ")
-heli(tekst,'et')
 
-
-
-kirjuta_failisse("Text.txt")
-
-paevad=loe_failist("Paevad.txt")
-print(paevad)
---
+++ TkinteriKasutamine.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import messagebox as mb
+
+k=0
+def vajuta():
+    global k
+    k+=1
+    nupp.configure(text=k)
+def vajuta_(event):
+    global k
+    k-=1
+    nupp.configure(text=k)
+def tst_psse(event):
+    t=textbox.get()
+    if t=="":
+        mb.showerror("Tähelepanu!","On vaja sisestada numbrid!")
+    else:
+        pealkiri.configure(text=t,width=len(t))
+        textbox.delete(0,END)
+        mb.showinfo("Aruanne","Tekst oli lisatud pealkijasse")
+def valik():
+    arv=var.get()
+    textbox.insert(END,arv)
+
+aken=Tk()
+aken.configure(bg="yellow")
+aken.geometry("400x600")
+aken.iconbitmap('icon.ico')
+aken.title("Tkinteri kasutamine")
+tekst="Pealkiri"
+pealkiri=Label(aken,
+               text=tekst,
+               bg="#ff7373",
+               fg="#133337",
+               font="Algerian 20",
+               height=3,width=len(tekst),
+               cursor="watch")
+textbox=Entry(aken,
+              bg="#133337",
+              fg="#ff7373",
+              font="Algerian 20",
+              width=20,
+              justify=CENTER) #show="*"
+nupp=Button(aken,
+            text="Vajuta mind!",
+            font="Arial 20",
+            height=3,width=10,
+            relief=RAISED,
+            bg="lightblue",
+            command=vajuta) #SUNKEN, GROOVE
+f=Frame(aken)
+var=IntVar() #StringVar()
+e=Radiobutton(f,text="Esimene",variable=var,value=1,font="Arial 20",command=valik)
+t=Radiobutton(f,text="Teine",variable=var,value=2,font="Arial 20",command=valik)
+k_=Radiobutton(f,text="Kolmas",variable=var,value=3,font="Arial 20",command=valik)
+nupp.bind("<Button-3>",vajuta_) #PKM
+textbox.bind("<Return>",tst_psse) #Enter
+
+obj=[pealkiri,textbox,nupp,f]
+for i in obj:
+    i.pack()
+obj2=[e,t,k_]
+for i in range(len(obj2)):
+    obj2[i].grid(row=0,column=i)
+
+aken.mainloop()
